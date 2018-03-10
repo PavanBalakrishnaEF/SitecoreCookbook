f8f4f9e8db4060d39384709360f064a6b2d9bb71
Pavan Balakrishna <pavan.balakrishna@ef.com>
2018-03-10 08:50:27 +0530

    Initial commit

---
+++ SitecoreCookbook/Controllers/CookBookController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SitecoreCookbook.Controllers
+{
+    public class CookBookController : Controller
+    {
+        // GET: CookBook
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
